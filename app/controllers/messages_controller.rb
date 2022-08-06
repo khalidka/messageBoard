31a5be62e13738b7109a10dc7b51e2199ec90fb7
Khalid Mursal <khalidka94@gmail.com>
2022-08-06 16:31:33 +0300

    add messageBoard to github

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,49 @@
+class MessagesController < ApplicationController
+    before_action :find_message, only:[:show, :edit, :update, :destroy]
+    def index
+        @messages = Message.all
+    end
+
+    def new
+        @message = current_user.messages.build
+    end
+
+    def show
+    end
+
+    def create
+        @message = current_user.messages.build(message_params)
+        if @message.save
+            redirect_to root_path
+        else
+            render new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @message.update(message_params)
+            redirect_to message_path
+        else
+            render edit
+        end
+    end
+
+    def destroy
+        @message.destroy
+        redirect_to root_path
+    end
+
+    private
+
+       def message_params
+         params.require(:message).permit(:title, :description, :user_id)
+       end
+
+       def find_message
+         @message = Message.find(params[:id])
+       end
+    
+end
